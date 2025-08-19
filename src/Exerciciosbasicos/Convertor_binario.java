182d062c7adb0300f89451b8163c3e2320535938
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-08-19 10:40:46 -0300

    Update Convertor_binario.java

--- src/Exerciciosbasicos/Convertor_binario.java
+++ src/Exerciciosbasicos/Convertor_binario.java
@@ -21,3 +21,34 @@ public class Convertor_binario {
 
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+package Exerciciosbasicos;
+
+public class Convertor_binario {
+
+    static String binario(int x) {
+        String bin = Integer.toBinaryString(x);
+        System.out.printf("Valor %d agora vale %s em binário\n", x, bin);
+        return bin; // retorna a string binária
+    }
+
+    static String hex(int x) {
+        String hex = Integer.toHexString(x);
+        System.out.printf("Valor %d agora vale %s em hexadecimal\n", x, hex);
+        return hex; // retorna a string hexadecimal
+    }
+
+    static String octal(int x) {
+        String oct = Integer.toOctalString(x);
+        System.out.printf("Valor %d agora vale %s em octal\n", x, oct);
+        return oct; // retorna a string octal
+    }
+
+    public static void main(String[] args) {
+        // chamando os métodos e guardando os retornos em variáveis
+        String b = Convertor_binario.binario(7);
+        String h = Convertor_binario.hex(141);
+        String o = Convertor_binario.octal(346);
+    }
+}
